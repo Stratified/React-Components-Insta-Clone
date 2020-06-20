5613ee1cb92cf8019966a1a27b851ed188a1be0b
Brian Bagrov <Brianbagrov@gmail.com>
2020-06-19 21:41:49 -0400

    MVP goals reached.

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,23 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
-import Post from "./Post";
-import "./Posts.css";
-// import data 
+import React, { useState } from 'react';
+import Post from './Post';
+import './Posts.css';
+import dummydata from '../../dummy-data';
+// import data
 
-const PostsPage = () => {
-  // set up state for your data
-  return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
-  );
+const PostsPage = (props) => {
+	// set up state for your data
+	const [data] = useState(dummydata);
+	console.log(data);
+	return (
+		<div className='posts-container-wrapper'>
+			{/* map through data here to return a Post and pass data as props to Post */}
+			{data.map((p) => (
+				<Post key={p.postID} post={p} />
+			))}
+		</div>
+	);
 };
 
 export default PostsPage;
